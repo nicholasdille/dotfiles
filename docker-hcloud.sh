fe1cb52755ce8361dca35106f8534dc1bef83ac3
Nicholas Dille <nicholas.dille@mailbox.org>
2018-12-04 12:56:27 +0100

    Updated

--- docker-hcloud.sh
+++ docker-hcloud.sh
@@ -7,7 +7,7 @@ if test -f "${CONFIG_FILE}"; then
     source "${CONFIG_FILE}"
 else
     HCLOUD_DOCKER_VM_NAME=$(date +"%Y%m%d%H%M")
-    hcloud server create --location fsn1 --image ubuntu-18.04 --name docker-${HCLOUD_DOCKER_VM_NAME} --ssh-key 116094 --type cx11 --user-data-from-file docker-user-data.txt
+    hcloud server create --location fsn1 --image ubuntu-18.04 --name docker-${HCLOUD_DOCKER_VM_NAME} --ssh-key 209622 --type cx11 --user-data-from-file ~/.config/docker-hcloud/docker-user-data.txt
     HCLOUD_DOCKER_VM_IP=$(hcloud server list --output columns=ipv4,name | grep docker-${HCLOUD_DOCKER_VM_NAME} | cut -d' ' -f1)
 
     cat >~/.config/docker-hcloud/vm <<EOF
@@ -17,7 +17,7 @@ EOF
 fi
 
 echo "Docker is being installed:"
-timeout 300 bash -c "while test -z '$(ssh root@${HCLOUD_DOCKER_VM_IP} ps -C dockerd --no-headers)'; do echo Waiting...; sleep 10; done"
+timeout 300 bash -c "while test -z '$(ssh -i id_rsa_hetzner root@${HCLOUD_DOCKER_VM_IP} ps -C dockerd --no-headers)'; do echo Waiting...; sleep 10; done"
 if test -z "$?"; then
     export DOCKER_HOST=ssh://root@${HCLOUD_DOCKER_VM_IP}
     echo Done.
