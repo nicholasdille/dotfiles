a956f934f7d46131e91af589c6239105f5c1e75a
Nicholas Dille <nicholas.dille@mailbox.org>
2018-12-11 17:10:41 +0100

    Added script to create SSH config files for hcloud server

---
+++ hcloud2ssh.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+hcloud server list -o columns=name,ipv4 | tail -n +2 | while read LINE
+do
+    SERVER_NAME=$(echo $LINE | awk '{print $1}')
+    SERVER_IP==$(echo $LINE | awk '{print $2}')
+
+    cat > ~/.ssh/config.d/${SERVER_NAME} <<EOF
+Host ${SERVER_NAME}
+    HostName ${SERVER_IP}
+    User root
+    IdentityFile ~/id_rsa_hetzner
+    StrictHostKeyChecking no
+    UserKnownHostsFile /dev/null
+EOF
+    chmod 0640 ~/.ssh/config.d/${SERVER_NAME}
+done
