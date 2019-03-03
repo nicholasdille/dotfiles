d10dc80fecf5ad234931c092d7871343b29b4ebe
Nicholas Dille <nicholas.dille@mailbox.org>
2019-03-03 22:30:32 +0100

    Updated creating a remote docker host

--- docker-hcloud.sh
+++ docker-hcloud.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
 
 mkdir -p ~/.config/docker-hcloud
-CONFIG_FILE=~/.config/docker-hcloud/vm
+CONFIG_FILE=~/.config/docker-hcloud/config.sh
+[[ -f "${CONFIG_FILE}" ]] && . ${CONFIG_FILE}
 
-if test -f "${CONFIG_FILE}"; then
-    source "${CONFIG_FILE}"
-else
-    HCLOUD_DOCKER_VM_NAME=$(date +"%Y%m%d%H%M")
-    hcloud server create --location fsn1 --image ubuntu-18.04 --name docker-${HCLOUD_DOCKER_VM_NAME} --ssh-key 209622 --type cx11 --user-data-from-file ~/.config/docker-hcloud/docker-user-data.txt
-    HCLOUD_DOCKER_VM_IP=$(hcloud server list --output columns=ipv4,name | grep docker-${HCLOUD_DOCKER_VM_NAME} | cut -d' ' -f1)
+HCLOUD_VM_IP=$(hcloud server list --selector docker-hcloud=true --output columns=ipv4 | tail -n +2)
+if [[ -z "${HCLOUD_VM_IP}" ]]; then
+    HCLOUD_VM_NAME="${VM_BASE_NAME}-$(date +%Y%m%d%H%M)"
+    hcloud server create \
+        --location ${HCLOUD_LOCATION} \
+        --image ${HCLOUD_IMAGE} \
+        --name ${HCLOUD_VM_NAME} \
+        --ssh-key ${HCLOUD_SSH_KEY} \
+        --type ${HCLOUD_TYPE} \
+        --user-data-from-file ~/.config/docker-hcloud/docker-user-data.txt
+    hcloud server add-label ${HCLOUD_VM_NAME} docker-hcloud=true
+    HCLOUD_VM_IP=$(hcloud server list --output columns=ipv4,name | grep docker-${HCLOUD_DOCKER_VM_NAME} | cut -d' ' -f1)
+fi
 
-    cat >~/.config/docker-hcloud/vm <<EOF
-    HCLOUD_DOCKER_VM_NAME=${HCLOUD_DOCKER_VM_NAME}
-    HCLOUD_DOCKER_VM_IP=${HCLOUD_DOCKER_VM_IP}
+# Creating SSH config
+cat >~/.ssh/config.d/docker-hcloud <<EOF
+Host ${HCLOUD_VM_IP}
+    HostName ${HCLOUD_VM_IP}
+    User root
+    IdentityFile ~/id_rsa_hetzner
+    StrictHostKeyChecking no
+    UserKnownHostsFile /dev/null
 EOF
-fi
+chmod 0600 ~/.ssh/config.d/docker-hcloud
 
-echo "Docker is being installed:"
-timeout 300 bash -c "while test -z '$(ssh -i id_rsa_hetzner root@${HCLOUD_DOCKER_VM_IP} ps -C dockerd --no-headers)'; do echo Waiting...; sleep 10; done"
-if test -z "$?"; then
-    export DOCKER_HOST=ssh://root@${HCLOUD_DOCKER_VM_IP}
-    echo Done.
-fi
+# Wait for dockerd
+echo Waiting for dockerd...
+timeout 300 bash -c "while test -z '$(ssh ${HCLOUD_VM_IP} ps -C dockerd --no-headers)'; do sleep 2; done"
+export DOCKER_HOST=ssh://${HCLOUD_VM_IP}
