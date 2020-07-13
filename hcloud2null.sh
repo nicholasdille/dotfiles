1eb9601b8a325e33566d7f1f9a56eb6f4d0d0048
Nicholas Dille <nicholas.dille@mailbox.org>
2020-07-13 20:38:25 +0200

    Removed outdated scripts

--- hcloud2null.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-hcloud volume list -o columns=id | tail -n +2 | xargs -r -n 1 hcloud volume detach
-hcloud volume list -o columns=id | tail -n +2 | xargs -r -n 1 hcloud volume delete
-hcloud server list -o columns=id | tail -n +2 | xargs -r -n 1 hcloud server delete
