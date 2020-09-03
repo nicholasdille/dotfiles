fc305399bd8ab8ea9f33e1dcb9e3ea31afe6b25a
Nicholas Dille <nicholas.dille@mailbox.org>
2020-09-03 09:04:18 +0200

    Updated WSL setup

--- .local/etc/profile.d/wsl.sh
+++ .local/etc/profile.d/wsl.sh
@@ -8,13 +8,11 @@ if [ $? -ne 0 ]; then
     ( setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",nofork & ) >/dev/null 2>&1
 fi
 
-GPG_SOCK="$HOME/.gnupg/S.gpg-agent"
+export GPG_SOCK="$HOME/.gnupg/S.gpg-agent"
 if ! ss -a | grep -q "$GPG_SOCK"; then
     rm -f "$GPG_SOCK"
     mkdir -p "$(dirname "$GPG_SOCK")"
-    setsid --fork socat UNIX-LISTEN:"$GPG_SOCK",fork EXEC:"npiperelay.exe -ei -ep -s -a "'"'"$APPDATA"/gnupg/S.gpg-agent'"',nofork
+    ( setsid socat UNIX-LISTEN:"$GPG_SOCK",fork EXEC:"npiperelay.exe -ei -ep -s -a "'"'"$APPDATA"/gnupg/S.gpg-agent'"',nofork & ) >/dev/null 2>&1
 fi
 
-if ! gpg-connect-agent "keyinfo --list" /bye; then
-    "/mnt/c/Program Files (x86)/GnuPG/bin/gpg-connect-agent.exe" /bye
-fi
+"/mnt/c/Program Files (x86)/GnuPG/bin/gpg-connect-agent.exe" /bye
