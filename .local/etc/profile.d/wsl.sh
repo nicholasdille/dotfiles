5f84741d63f1116e07236414d24e46c0125af9e3
Nicholas Dille <nicholas.dille@mailbox.org>
2020-09-25 08:51:45 +0200

    Updates

--- .local/etc/profile.d/wsl.sh
+++ .local/etc/profile.d/wsl.sh
@@ -9,7 +9,7 @@ if test -n "${WSL_DISTRO_NAME}"; then
         ( setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",nofork & ) >/dev/null 2>&1
     fi
 
-    GPG_SOCK="$HOME/.gnupg/S.gpg-agent"
+    export GPG_SOCK="$HOME/.gnupg/S.gpg-agent"
     if ! ss -a | grep -q "$GPG_SOCK"; then
         rm -f "$GPG_SOCK"
         mkdir -p "$(dirname "$GPG_SOCK")"
