d108e7d2579d4f00929dd151039a8f896f687a8b
Nicholas Dille <nicholas.dille@mailbox.org>
2020-11-03 08:00:23 +0100

    Fixed umbrella

--- .local/etc/profile.d/umbrella.sh
+++ .local/etc/profile.d/umbrella.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-set -o errexit
 
 if test -z "${WSL_DISTRO_NAME}"; then
     exit
 fi
 
-sudo true
-
 if ! test -f /usr/local/share/ca-certificates/umbrella.crt; then
+    sudo true
     powershell.exe -command "Get-ChildItem cert:\\LocalMachine\\Root | Where-Object {\$_.Subject -eq 'CN=Cisco Umbrella Root CA, O=Cisco'} | Export-Certificate -Type CERT -FilePath \\\\wsl\$\\${WSL_DISTRO_NAME}\\home\\${USER}\\umbrella.cer"
 
     if test -f "${HOME}/umbrella.cer"; then
