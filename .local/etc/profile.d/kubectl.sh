8665c153502c673cded3212479f55c9e4eaa5b17
Nicholas Dille <nicholas.dille@mailbox.org>
2020-12-01 10:13:05 +0100

    Removed trailing colon

--- .local/etc/profile.d/kubectl.sh
+++ .local/etc/profile.d/kubectl.sh
@@ -1,3 +1,4 @@
-export KUBECONFIG=~/.kube/config:$(ls ~/.kube/{kube,}config.* 2>/dev/null | tr '\n' ':')
+KUBECONFIG_FILES=$(ls ~/.kube/{kube,}config.* 2>/dev/null)
+export KUBECONFIG=~/.kube/config:$(echo ${KUBECONFIG_FILES} | tr ' ' ':')
 alias k=kubectl
 complete -F __start_kubectl k
