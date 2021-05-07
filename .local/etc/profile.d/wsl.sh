18141c9ba87ad81722fa6cdce96681b837132414
Nicholas Dille <nicholas.dille@mailbox.org>
2021-05-07 14:09:25 +0200

    Updates

--- .local/etc/profile.d/wsl.sh
+++ .local/etc/profile.d/wsl.sh
@@ -1,18 +1,24 @@
 if test -n "${WSL_DISTRO_NAME}"; then
 
-    export SSH_AUTH_SOCK=$HOME/.ssh/agent.sock
-    if ! ss -a | grep -q $SSH_AUTH_SOCK; then
-        rm -f $SSH_AUTH_SOCK
-        ( setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",nofork & ) >/dev/null 2>&1
+    export SSH_AUTH_SOCK="${HOME}/.ssh/agent.sock"
+    if ! ss -a | grep -q "${SSH_AUTH_SOCK}"; then
+        echo "Setting up socket for SSH agent"
+
+        rm -f "${SSH_AUTH_SOCK}"
+        mkdir -p "$(dirname "${SSH_AUTH_SOCK}")"
+        ( setsid socat UNIX-LISTEN:"${SSH_AUTH_SOCK}",fork EXEC:"npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",nofork & ) >/dev/null 2>&1
     fi
 
-    export GPG_SOCK="$HOME/.gnupg/S.gpg-agent"
-    if ! ss -a | grep -q "$GPG_SOCK"; then
+    export GPG_SOCK="${HOME}/.gnupg/S.gpg-agent"
+    if ! ss -a | grep -q "${GPG_SOCK}"; then
+        echo "Setting up socket for GPG agent"
+
         APPDATA="$(wslvar appdata)"
         APPDATA="${APPDATA//\\/\/}"
-        rm -f "$GPG_SOCK"
-        mkdir -p "$(dirname "$GPG_SOCK")"
-        ( setsid socat UNIX-LISTEN:"$GPG_SOCK",fork EXEC:"npiperelay.exe -ei -ep -s -a "'"'"$APPDATA"/gnupg/S.gpg-agent'"',nofork & ) >/dev/null 2>&1
+
+        rm -f "${GPG_SOCK}"
+        mkdir -p "$(dirname "${GPG_SOCK}")"
+        ( setsid socat UNIX-LISTEN:"${GPG_SOCK}",fork EXEC:"npiperelay.exe -ei -ep -s -a "'"'"${APPDATA}"/gnupg/S.gpg-agent'"',nofork & ) >/dev/null 2>&1
     fi
 
     "/mnt/c/Program Files (x86)/GnuPG/bin/gpg-connect-agent.exe" /bye
