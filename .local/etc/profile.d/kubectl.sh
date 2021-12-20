4e2a6af0cf247e9453826a24b6ee4c7c4b4a70e4
Nicholas Dille <nicholas.dille@mailbox.org>
2021-12-20 23:44:50 +0100

    Updates

--- .local/etc/profile.d/kubectl.sh
+++ .local/etc/profile.d/kubectl.sh
@@ -3,7 +3,6 @@
 if type kubectl >/dev/null 2>&1; then
     KUBECONFIG_FILES=$(ls ${HOME}/.kube/{kube,}config.* 2>/dev/null)
     export KUBECONFIG=${HOME}/.kube/config:$(echo ${KUBECONFIG_FILES} | tr ' ' ':')
-    alias k=kubectl
     complete -F __start_kubectl k
 fi
 
