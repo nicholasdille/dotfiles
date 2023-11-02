4217d3ca26d7e80658947bf6307ddf29693b78d6
Nicholas Dille <nicholas.dille@mailbox.org>
2023-11-02 10:19:44 +0100

    Fixed PATH for umbrella

--- .local/etc/profile.d/umbrella.sh
+++ .local/etc/profile.d/umbrella.sh
@@ -3,6 +3,8 @@
 if test -n "${WSL_DISTRO_NAME}" && test "${HOSTNAME:0:2}" == "HG"; then
 
     if ! test -f /usr/local/share/ca-certificates/umbrella.crt; then
+        PATH="${PATH:+${PATH}:}/mnt/c/Windows/System32/WindowsPowerShell/v1.0"
+
         sudo true
         powershell.exe -command "Get-ChildItem cert:\\LocalMachine\\Root | Where-Object {\$_.Subject -eq 'CN=Cisco Umbrella Root CA, O=Cisco'} | Export-Certificate -Type CERT -FilePath \\\\wsl\$\\${WSL_DISTRO_NAME}\\home\\${USER}\\umbrella.cer"
 
