bf86c88a797324c726f6cb8a00a2de0a7c41a3da
Nicholas Dille <nicholas.dille@mailbox.org>
2023-11-10 07:17:11 +0100

    Fixed MTU

--- .local/etc/profile.d/mtu.sh
+++ .local/etc/profile.d/mtu.sh
@@ -4,12 +4,19 @@ if test -n "${WSL_DISTRO_NAME}" && test "${HOSTNAME:0:2}" == "HG"; then
 
     PATH="${PATH:+${PATH}:}/mnt/c/Windows/System32/WindowsPowerShell/v1.0"
 
-    sudo true
     MIN_MTU="$(
         powershell.exe -command "Get-NetIPInterface | Select-Object -ExpandProperty NlMtu | Measure-Object -Minimum | Select-Object -ExpandProperty Minimum" \
         | tr -d '\r'
     )"
-    echo "Got minimum MTU ${MIN_MTU}"
-    sudo ip link set dev eth0 mtu ${MIN_MTU}
+    CUR_MTU="$(
+        ip link show dev eth0 \
+        | grep mtu \
+        | cut -d' ' -f5
+    )"
+
+    if test "${CUR_MTU}" -ne "${MIN_MTU}"; then
+        echo "Setting MTU to ${MIN_MTU}. Sudo required."
+        sudo ip link set dev eth0 mtu ${MIN_MTU}
+    fi
 
 fi
