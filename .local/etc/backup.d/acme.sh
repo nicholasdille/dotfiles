fe7ffcfba171441b80c462e7148ffa8386f30839
Nicholas Dille <nicholas.dille@mailbox.org>
2024-02-19 23:05:20 +0100

    Updates

--- .local/etc/backup.d/acme.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -o errexit
-
-if test -d "${HOME}/.acme.shls -l "; then
-    echo "${HOME}/.acme.shls -l "
-fi
---
+++ .local/etc/backup.d/private.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -o errexit
+
+if test -d "${HOME}/private"; then
+    echo "${HOME}/private"
+fi
